67cb3b1b3b7b3d0be29cf9e59dfbd2e712c6bdc2
Shrey <shrey7shrey@gmail.com>
2025-03-27 00:41:14 +0530

    completed load.py

--- etl/load.py
+++ etl/load.py
@@ -12,6 +12,24 @@ DB_USER = os.getenv('DB_USER', 'shreysharma')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'shrey123')
 DB_PORT = os.getenv('DB_PORT', '5432')
 
-def connect_db()
+def connect_db():
     try:
-        engine = 
+        engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+        print('Connected to the database')
+        return engine
+    except Exception as e:
+        print('Error connecting to the database', e)
+        return None
+
+def load_data_to_db(file_path = 'data/processed/customers_cleaned.csv', table_name='customer_churn'):
+    try:
+        df = pd.read_csv(file_path)
+        engine = connect_db()
+        if engine is not None:
+            df.to_sql(table_name, engine, if_exists='replace', index=False)
+            print('Data loadded successfully into the table')
+    except Exception as e:
+        print('Error loading the data into the table', e)
+
+if __name__ == '__main__':
+    load_data_to_db()
